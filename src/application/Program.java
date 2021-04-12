6c91c196086b40464fcda5f25c4ae96c710701f2
Andreza Mohamad <andrezamohamad@outlook.com>
2021-04-12 18:32:08 -0300

    Exercicio70

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee employee = new Employee();
+		
+		System.out.print("Insira o nome do funcionario: ");
+		employee.name = sc.nextLine();
+		
+		System.out.print("Insira o salário bruto do funcionario: ");
+		employee.grossSalary = sc.nextDouble();
+		
+		System.out.print("Insira o desconto: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println ("Salario Liquido: " + employee);
+		
+		/*
+		System.out.println("Nome: " + employee.name);
+		System.out.println("Salario Bruto: " + employee.grossSalary);
+		System.out.println("Salario Liquido: " + employee.netSalary); */
+		
+		System.out.println();
+		System.out.println ("Insira a porcentagem para aumento: ");
+		double percentage = sc.nextDouble();
+		employee.increaseSalary(percentage);
+		
+		System.out.println();
+		System.out.println ("Salario final: " + employee);
+		
+		sc.close();
+		
+
+	}
+
+}
